fadc818b549a3215a95ea5d121aace326244a753
chaos-03x <a131139899@gmail.com>
2025-03-20 22:02:57 +0800

    update

--- src/main/index.js
+++ src/main/index.js
@@ -1,19 +1,22 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 const path = require('path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 600,
+        width: 1200,
         height: 800,
         webPreferences: {
             preload: path.join(__dirname + 'preload.js'),
             nodeIntegration: false,
             contextIsonlation: true,
         },
+        
     })
+    
+    Menu.setApplicationMenu(null);
 
-    win.loadURL('http://localhost:5173'); // 加载 Vite 本地服务器
-    // win.loadFile('public/index.html');
+    win.loadURL('http://localhost:5173'); // 开发环境
+    // win.loadFile('dist/index.html');
 
 }
 
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   root: './src/renderer', // 设置渲染进程为根目录
+  publicDir: './src/public',
   base: './',
   build: {
     outDir: '../../dist', // 编译输出到 dist 目录
