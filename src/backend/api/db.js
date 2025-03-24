c6f0bd16a8669e8453b371cc68ac28ed7457fc2d
chaos-03x <a131139899@gmail.com>
2025-03-24 23:48:18 +0800

    update

--- src/backend/api/db.js
+++ src/backend/api/db.js
@@ -6,24 +6,36 @@ const mangaModel = require('../db/models/manga.js');
 const metadataModel = require('../db/models/metadata.js');
 const db = require('../db/index.js');
 
-// 获取所有漫画
+/**
+ * 获取所有漫画
+ * @returns {Array} - 返回所有漫画的数组
+ */
 function getMangas() {
     return mangaModel.getAllMangas();
 }
 
-// 获取漫画详情
+/**
+ * 获取漫画的详细信息
+ * @param {Number} id - 漫画表的ID
+ * @returns {Object} - 返回指定ID的漫画和对应的元数据
+ */
 function getMangaDetails(id) {
     const manga = mangaModel.getMangaById(id);
-    if (!manga) return null; // 如果没有找到漫画，返回null
+    if (!manga) return null;
     const metadata = metadataModel.getMetadataByMangaId(id);
-    return { ...manga, metadata }; // 返回漫画和对应的元数据
+    return { ...manga, metadata };
 }
 
-// 添加新漫画
+/**
+ * 添加新的漫画
+ * @param {Object} data - 漫画的本地信息对象
+ * @param {Object} metadata - 漫画的元数据对象
+ * @returns {Number} - 新添加的漫画ID
+ */
 function addNewManga(data, metadata) {
     const { title, path, pages, size } = data;
     const { title: metaTitle, alt_title, language, category, group_name, character, hitomi_id, artist, parody, tags } = metadata || {};
-    // 如果没有提供元数据，则使用默认值
+    // 如果没有提供元数据，则默认使用空值
     
     try {
         const transaction = db.transaction(() => {
@@ -35,7 +47,7 @@ function addNewManga(data, metadata) {
             return mangaId;
         });
     
-        return transaction(); // 执行事务并返回新添加的漫画ID
+        return transaction();
     } catch (error) {
         console.error('添加漫画失败:', error);
         throw error; 
--- src/backend/utils/file.js
+++ src/backend/utils/file.js
@@ -1,3 +1,13 @@
 /**
  * 文件和目录操作工具。
  */
+
+const fs = require('fs/promises');
+const path = require('path');
+
+/**
+ * 获取指定路径下的所有文件路径
+ */
+async function getDirs(dirPath) {
+    
+}
