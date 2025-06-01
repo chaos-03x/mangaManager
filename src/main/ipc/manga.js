9bda31d2f7ec2b4f8033213958beb2a86c9698cb
chaos-03x <a131139899@gmail.com>
2025-06-01 14:07:45 +0800

    update

--- src/main/ipc/manga.js
+++ src/main/ipc/manga.js
@@ -1,12 +1,28 @@
-const {ipcMain} = require('electron')
+const { ipcMain } = require('electron')
 const path = require('path')
-const fs = require('fs')
+const fs = require('fs/promises')
 
-function scanMangas(libraryPath = "C:/Users/z.chaos/Downloads/Test"){
+const libraryPath = "F:/Test"
+// const libraryPath = "C:/Users/z.chaos/Downloads/Test"
+// 步骤 A：找到所有的作者文件夹。
+// 步骤 B：对于每个作者，找到其名下所有的漫画系列文件夹。
+// 步骤 C：对于每个漫画系列，提取漫画名和 ID。
+// 步骤 D：对于每个漫画系列，统计里面的 .webp 图片数量。
+// 步骤 E：将提取到的信息（作者、漫画名、ID、图片数、路径）整理并收集起来。
+
+async function scanMangas(libraryPath) {
     try {
-        
+        // const a = await fs.readdir(libraryPath)
+        // const b = await fs.readdir(libraryPath,{withFileTypes:true})
+
+        const authorPath = await fs.readdir(libraryPath)
+
+
+        console.log(authorPath);;
+
     } catch (error) {
-        console.log('扫描失败：',error)
-        throw new Error(`扫描失败：${error.message}`)
+        console.log('扫描漫画库失败:',error);
     }
 }
+
+scanMangas(libraryPath)
